22781abaee2c179b0edef913b800fcafd916c04b
Rimantas <belauzas@gmail.com>
2023-11-06 11:44:12 +0200

    Import/export example

---
+++ js/main.js
@@ -0,0 +1,16 @@
+// LIETUVA - automobilis, sunkvezimis
+import { minus, multi, sum } from './sum.js';
+
+console.log('labas');
+
+const n1 = 7;
+const n2 = 5;
+
+
+const n12 = sum(n1, n2);
+const n12minus = minus(n1, n2);
+const n12multi = multi(n1, n2);
+
+console.log(n12);
+console.log(n12minus);
+console.log(n12multi);
---
+++ js/sum.js
@@ -0,0 +1,18 @@
+function sum(a, b) {
+    return a + b;
+}
+
+function minus(a, b) {
+    return a - b;
+}
+
+function div(a, b) {
+    return a / b;
+}
+
+function multi(a, b) {
+    return a * b;
+}
+
+// export default sum;
+export { sum, minus, div, multi };
